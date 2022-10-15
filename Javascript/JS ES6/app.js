f9710de3d16d6da1f3089109c3bd3b019ca4ac75
Juansa77 <dedalus1977@gmail.com>
2022-10-15 20:39:37 +0200

    complete

--- Javascript/JS ES6/app.js
+++ Javascript/JS ES6/app.js
@@ -8,17 +8,15 @@ por consola la suma de los dos parametros.
 1.2 Ejecuta esta función pasando un solo parametro
 1.3 Ejecuta esta función pasando dos parametros*/
 
-const arrowFunk = (a, b) =>{
-a= 10;
-b =5;
-console.log((a + b))
+const arrowFunk = (a, b) => {
+  a = 10;
+  b = 5;
+  console.log(a + b);
+};
 
-}
-
-arrowFunk()
-arrowFunk(2)
-arrowFunk(2, 7)
-
+arrowFunk();
+arrowFunk(2);
+arrowFunk(2, 7);
 
 /*Iteración #2: Destructuring
 
@@ -26,18 +24,20 @@ arrowFunk(2, 7)
 del objeto usando object destructuring e imprimelas por consola. Cuidado, 
 no hace falta hacer destructuring del array, solo del objeto.*/
 
+const game = {
+  title: "The last us 2",
+  gender: ["action", "zombie", "survival"],
+  year: 2020,
+};
 
-const game = {title: 'The last us 2', gender: ['action', 'zombie', 'survival'], year: 2020}
-
-const { title, gender, year} = game;
-console.log(title, gender, year) 
-
+const { title, gender, year } = game;
+console.log(title, gender, year);
 
 /*2.2 En base al siguiente javascript, usa destructuring para crear 3 variables 
 llamadas fruit1, fruit2 y fruit3, con los valores del array. Posteriormente
 imprimelo por consola.*/
 
-const fruits = ['Banana', 'Strawberry', 'Orange'];
+const fruits = ["Banana", "Strawberry", "Orange"];
 const [fruit1, fruit2, fruit3] = fruits;
 console.log(fruit1, fruit2, fruit3);
 
@@ -47,161 +47,159 @@ console.log(fruit1, fruit2, fruit3);
 variables igualandolo a la función e imprimiendolo por consola.*/
 
 const animalFunction = () => {
-    return {name: 'Bengal Tiger', race: 'Tiger'}
+  return { name: "Bengal Tiger", race: "Tiger" };
 };
 
-const {name, race} = animalFunction();
+const { name, race } = animalFunction();
 
-console.log( name +race)
+console.log(name + race);
 
 /*2.4 En base al siguiente javascript, usa destructuring para crear las 
 variables name y itv con sus respectivos valores. Posteriormente crea 
 3 variables usando igualmente el destructuring para cada uno de los años 
 y comprueba que todo esta bien imprimiendolo.*/
 
-const car = {name: 'Mazda 6', itv: [2015, 2011, 2020] }
-const {name2, itv} = car;
+const car = { name: "Mazda 6", itv: [2015, 2011, 2020] };
+const { name2, itv } = car;
 
 const [year1, year2, year3] = itv;
-console.log(year1, year2,  year3)
+console.log(year1, year2, year3);
 
 ///Iteración #3: Spread Operator///
 
-
 /*3.1 Dado el siguiente array, crea una copia usando spread operators.*/
 
-const pointsList = [32, 54, 21, 64, 75, 43]
+const pointsList = [32, 54, 21, 64, 75, 43];
 const doa = [...pointsList];
-console.log(doa)
-
+console.log(doa);
 
 /*3.2 Dado el siguiente objeto, crea una copia usando spread operators.*/
-const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
-const toy2 = {...toy};
-console.log(toy2)
+const toy = { name: "Bus laiyiar", date: "20-30-1995", color: "multicolor" };
+const toy2 = { ...toy };
+console.log(toy2);
 
 /*3.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando 
 spread operatos.*/
 const pointsListD = [32, 54, 21, 64, 75, 43];
-const pointsLis2 = [54,87,99,65,32];
+const pointsLis2 = [54, 87, 99, 65, 32];
 const sumArray = [...pointsListD, ...pointsLis2];
-console.log(sumArray)
+console.log(sumArray);
 
 /*3.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos 
 con spread operators.*/
-const toyD = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
-const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']}
+const toyD = { name: "Bus laiyiar", date: "20-30-1995", color: "multicolor" };
+const toyUpdate = { lights: "rgb", power: ["Volar like a dragon", "MoonWalk"] };
 
-const toyThree = {...toyD, ...toyUpdate};
-console.log(toyThree)
+const toyThree = { ...toyD, ...toyUpdate };
+console.log(toyThree);
 
 /*3.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2 
 pero sin editar el array inicial. De nuevo, usando spread operatos.*/
 
-const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
-const [ rojo, azul, amarillo, verde, naranja] = colors;
-const final = [...rojo, ...azul, ...verde, ...naranja]
-console.log(final) 
+const colors = ["rojo", "azul", "amarillo", "verde", "naranja"];
+const [rojo, azul, amarillo, verde, naranja] = colors;
+const final = [...rojo, ...azul, ...verde, ...naranja];
+console.log(final);
 
 //Iteración #4: Map//
 
 /*4.1 Dado el siguiente array, devuelve un array con sus nombres 
 utilizando .map().*/
 const users = [
-	{id: 1, name: 'Abel'},
-	{id:2, name: 'Julia'},
-	{id:3, name: 'Pedro'},
-	{id:4, name: 'Amanda'}
-]
+  { id: 1, name: "Abel" },
+  { id: 2, name: "Julia" },
+  { id: 3, name: "Pedro" },
+  { id: 4, name: "Amanda" },
+];
 
-const namesP= users.map(element => element.name)
-console.log(namesP)
+const namesP = users.map((element) => element.name);
+console.log(namesP);
 
 /*4.2 Dado el siguiente array, devuelve una lista que contenga los valores 
 de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que 
 empiece por 'A'.*/
 
-var namesPdos = []
+var namesPdos = [];
 
-namesPdos= namesP.map((element) => {
-  if(element.startsWith("A")) {
-   return "Anacleto";}else{return element}}
-    
-);
-console.log(namesPdos)
+namesPdos = namesP.map((element) => {
+  if (element.startsWith("A")) {
+    return "Anacleto";
+  } else {
+    return element;
+  }
+});
+console.log(namesPdos);
 
 /*4.3 Dado el siguiente array, devuelve una lista que contenga los valores 
 de la propiedad .name y añade al valor de .name el string ' (Visitado)' 
 cuando el valor de la propiedad isVisited = true.*/
 
-
 const cities = [
-	{isVisited:true, name: 'Tokyo'}, 
-	{isVisited:false, name: 'Madagascar'},
-	{isVisited:true, name: 'Amsterdam'}, 
-	{isVisited:false, name: 'Seul'}
-]
+  { isVisited: true, name: "Tokyo" },
+  { isVisited: false, name: "Madagascar" },
+  { isVisited: true, name: "Amsterdam" },
+  { isVisited: false, name: "Seul" },
+];
 
 var citiesArray = {};
 
-citiesArray= cities.map((element) =>{ 
-	if(element.isVisited == true){return element.name + " (visitado)";}
+citiesArray = cities.map((element) => {
+  if (element.isVisited == true) {
+    return element.name + " (visitado)";
+  } else {
+    return element.name;
+  }
+});
 
-	else {return element.name}
-
-
-})
-
-console.log(citiesArray)
+console.log(citiesArray);
 
 /*Iteración #5: Filter*/
-
 
 /*5.1 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
 con los valores que sean mayor que 18*/
 
 const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
 
-const adult= ages.filter(element => element > 18)
-console.log(adult)
-
+const adult = ages.filter((element) => element > 18);
+console.log(adult);
 
 /*5.2 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
 con los valores que sean par.*/
 
 const agesTwo = [22, 14, 24, 55, 65, 21, 12, 13, 90];
-const pares=agesTwo.filter(element =>  element % 2 == 0)
+const pares = agesTwo.filter((element) => element % 2 == 0);
 console.log(pares);
-
 
 /*5.3 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
 con los streamers que tengan el gameMorePlayed = 'League of Legends'.*/
 
 const streamers = [
-	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
-	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'}, 
-	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
-	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+  { name: "Rubius", age: 32, gameMorePlayed: "Minecraft" },
+  { name: "Ibai", age: 25, gameMorePlayed: "League of Legends" },
+  { name: "Reven", age: 43, gameMorePlayed: "League of Legends" },
+  { name: "AuronPlay", age: 33, gameMorePlayed: "Among Us" },
 ];
 
+const mostGamer = streamers.filter(
+  (element) => element.gameMorePlayed == "League of Legends"
+);
 
-const mostGamer=streamers.filter(element => 
- element.gameMorePlayed ==  "League of Legends")
-
- console.log(mostGamer)
+console.log(mostGamer);
 
 /*5.4 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
 con los streamers que incluyan el caracter 'u' en su propiedad .name. Recomendamos 
 usar la funcion .includes() para la comprobación.*/
 const streamersTwo = [
-	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
-	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
-	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
-	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+  { name: "Rubius", age: 32, gameMorePlayed: "Minecraft" },
+  { name: "Ibai", age: 25, gameMorePlayed: "League of Legends" },
+  { name: "Reven", age: 43, gameMorePlayed: "League of Legends" },
+  { name: "AuronPlay", age: 33, gameMorePlayed: "Among Us" },
 ];
 
-const streamWithU= streamersTwo.filter(element =>element.name.includes("u"))
-console.log(streamWithU)
+const streamWithU = streamersTwo.filter((element) =>
+  element.name.includes("u")
+);
+console.log(streamWithU);
 
 /*
 5.5 utiliza .filter() para generar un nuevo array con los streamers que incluyan 
@@ -210,38 +208,46 @@ el caracter 'Legends' en su propiedad .gameMorePlayed. Recomendamos usar la func
 Además, pon el valor de la propiedad .gameMorePlayed a MAYUSCULAS cuando 
 .age sea mayor que 35.*/
 
-const streamerLegend = streamersTwo.filter((element) => {element.gameMorePlayed.includes("Legends") 
- if(element.age> 35) {element.gameMorePlayed.toUpperCase()}
+const streamerLegend = streamersTwo.filter((element) => {
+  element.gameMorePlayed.includes("Legends");
+  if (element.age > 35) {
+    element.gameMorePlayed.toUpperCase();
+  }
+});
 
-
-})
-
-
-console.log(streamerLegend)
+console.log(streamerLegend);
 
 /*5.6 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola 
 los streamers que incluyan la palabra introducida en el input. De esta forma, si 
 introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si
 introduzco 'i', me deberia de mostrar el streamer 'Rubius' e 'Ibai'.*/
 const streamersThree = [
-	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
-	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
-	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
-	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+  { name: "Rubius", age: 32, gameMorePlayed: "Minecraft" },
+  { name: "Ibai", age: 25, gameMorePlayed: "League of Legends" },
+  { name: "Reven", age: 43, gameMorePlayed: "League of Legends" },
+  { name: "AuronPlay", age: 33, gameMorePlayed: "Among Us" },
 ];
 
 const toFilterStreamers = (string) => {
-	string= document.getElementById("letra")
-    let filtered=streamersThree.filter(element => element.name.includes(string))
-	alert(filtered)
+  //string= document.getElementById("letra")
+  let filtered = streamersThree.filter((element) =>
+    element.name.includes(string)
+  );
+  alert(filtered);
+};
 
-
-
+//var input= document.getElementById("theInput")
+//var toFilter = document.getElementById("theInput").value
+function toFilterStreamersTwo(toFilter) {
+  let filtered = streamersThree.filter((element) =>
+    element.name.includes(toFilter)
+  );
+  return filtered;
 }
 
-
-
-
+function toConsole() {
+  console.log(toFilterStreamersTwo(toFilter));
+}
 
 /*</meta>5.7 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola 
 los streamers que incluyan la palabra introducida en el input. De esta forma, si 
@@ -255,16 +261,131 @@ const streamers = [
 	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
 ];
 
-<!doctype html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-             <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-             <meta http-equiv="X-UA-Compatible" content="ie=edge">
-             <title>Document</title>
-</head>
-<body>
-  <input type="text" data-function="toFilterStreamers"/>
-  <button data-function="toShowFilterStreamers">Filter</button>
-</body>
-</html>*/
+*/
+
+/*6.1 Dado el siguiente array, usa .find() para econtrar el número 100.*/
+const numbers = [32, 21, 63, 95, 100, 67, 43];
+const finder = numbers.find((element) => element > 99);
+console.log(finder);
+
+/*6.2 Dado el siguiente array, usa .find() para econtrar la pelicula del año 2010.*/
+
+const movies = [
+  { title: "Madagascar", stars: 4.5, date: 2015 },
+  { title: "Origen", stars: 5, date: 2010 },
+  { title: "Your Name", stars: 5, date: 2016 },
+];
+
+const finderMovie = movies.find(({ date }) => date == 2010);
+console.log(finderMovie);
+
+/*6.3 Dado el siguiente javascript, usa .find() para econtrar el alien de nombre 
+'Cucushumushu' y la mutación 'Porompompero'. Una vez que los encuentres, usa 
+spread operator para fusionarlos teniendo en cuenta que el objeto de la mutación 
+lo queremos meter en la propiedad .mutation del objeto fusionado.*/
+
+const aliens = [
+  { name: "Zalamero", planet: "Eden", age: 4029 },
+  { name: "Paktu", planet: "Andromeda", age: 32 },
+  { name: "Cucushumushu", planet: "Marte", age: 503021 },
+];
+
+const mutations = [
+  {
+    name: "Porompompero",
+    description:
+      "Hace que el alien pueda adquirir la habilidad de tocar el tambor",
+  },
+  {
+    name: "Fly me to the moon",
+    description: "Permite volar, solo y exclusivamente a la luna",
+  },
+  {
+    name: "Andando que es gerundio",
+    description: "Invoca a un señor mayor como Personal Trainer",
+  },
+];
+
+const finderAlien = aliens.find(({ name }) => name == "Cucushumushu");
+const finderMutation = mutations.find(({ name }) => name == "Porompompero");
+const sumArrayS = { ...finderAlien, mutations: finderMutation.name };
+console.log(sumArrayS);
+
+/*Iteración #7: Reduce*/
+
+/*7.1 Dado el siguiente array, haz una suma de todos las notas de los examenes de 
+los alumnos usando la función .reduce().*/
+
+const exams = [
+  { name: "Yuyu Cabeza Crack", score: 5 },
+  { name: "Maria Aranda Jimenez", score: 1 },
+  { name: "Cristóbal Martínez Lorenzo", score: 6 },
+  { name: "Mercedez Regrera Brito", score: 7 },
+  { name: "Pamela Anderson", score: 3 },
+  { name: "Enrique Perez Lijó", score: 6 },
+  { name: "Pedro Benitez Pacheco", score: 8 },
+  { name: "Ayumi Hamasaki", score: 4 },
+  { name: "Robert Kiyosaki", score: 2 },
+  { name: "Keanu Reeves", score: 10 },
+];
+
+let valorInicial = 0;
+
+const sum = exams.reduce(function (acuu, curr) {
+  return acuu + curr.score;
+}, valorInicial);
+console.log(sum);
+
+/*
+7.2 Dado el mismo array, haz una suma de todos las notas de los examenes de los 
+alumnos que esten aprobados usando la función .reduce().*/
+const aprobados = [];
+const todos = [];
+
+for (const key of exams) {
+  if (key.score >= 5) aprobados.push(key.score);
+}
+
+for (const iterator of exams) {
+  todos.push(iterator.score);
+}
+var sumaAprobador = aprobados.reduce(
+  (acumulator, current) => acumulator + current
+);
+console.log(sumaAprobador);
+
+/*7.3 Dado el mismo array, haz la media de las notas de todos los examenes .reduce().*/
+const media = todos.reduce((acuu, curr, media) => {
+  return (acuu + curr) / media;
+});
+console.log(media);
+
+/*6.1 Dado el siguiente javascript filtra los videojuegos por gender = 'RPG' usando 
+.filter() y usa .reduce() para conseguir la media de sus .score. 
+La función .find() también podría ayudarte para el contrar el genero 'RPG' en el 
+array .gender.*/
+
+const videogames = [
+  { name: "Final Fantasy VII", genders: ["RPG"], score: 9.5 },
+  { name: "Assasins Creed Valhala", genders: ["Aventura", "RPG"], score: 4.5 },
+  { name: "The last of Us 2", genders: ["Acción", "Aventura"], score: 9.8 },
+  { name: "Super Mario Bros", genders: ["Plataforma"], score: 8.5 },
+  { name: "Genshin Impact", genders: ["RPG", "Aventura"], score: 7.5 },
+  {
+    name: "Legend of Zelda: Breath of the wild",
+    genders: ["RPG", "La cosa más puto bonita que he visto nunca"],
+    score: 10,
+  },
+];
+
+const rpg = videogames.filter((element) => element.genders.includes("RPG"));
+const notasVG = [];
+console.log(rpg)
+for (const iterator of rpg) {
+  notasVG.push(iterator.score);
+}
+
+const mediaVg = notasVG.reduce((acuu, curr, media) => {
+  return ( (acuu + curr) / media);
+});
+console.log(mediaVg);
