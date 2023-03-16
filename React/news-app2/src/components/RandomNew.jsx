934ad1fd27fe99b1472cf7a9efe6e3209e54fd33
Juansa77 <dedalus1977@gmail.com>
2023-03-16 18:56:55 +0100

    render optimization in component

--- React/news-app2/src/components/RandomNew.jsx
+++ React/news-app2/src/components/RandomNew.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import "./RandomNew.css"
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 const RandomNew = ({data}) => {
-    var randomNew = Math.floor(Math.random()*data.length)
+
+  const [randomIndex, setRandomIndex] =useState(0)
+  var newsLength = data.length
+  
+
+
+    useEffect(()=>{
+
+      setRandomIndex(Math.floor(Math.random()*newsLength))
+    },[newsLength])
   return (
-    <div className="randomNew">{randomNew !=0 &&
-        <Link to={`/noticias/${data[randomNew].topic}/${data[randomNew].id}`}>
+    <div className="randomNew">{randomIndex !=0 &&
+        <Link to={`/noticias/${data[randomIndex].topic}/${data[randomIndex].id}`}>
         <figure className="cardRandom">
-          <h5>{data[randomNew].topic}</h5>
-          <img src={data[randomNew].img} />
-          <h1>{data[randomNew].title}</h1>
-          <h3>{data[randomNew].author}</h3>
+          <h5>{data[randomIndex].topic}</h5>
+          <img src={data[randomIndex].img} />
+          <h1>{data[randomIndex].title}</h1>
+          <h3>{data[randomIndex].author}</h3>
           <p>Esse elit do aute consequat tempor nostrud reprehenderit eiusmod
       pariatur dolore fugiat duis. Pariatur sunt qui adipisicing quis
       culpa sint deserunt quis enim dolore. Eiusmod reprehenderit officia
