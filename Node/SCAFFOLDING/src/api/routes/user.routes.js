2aeb0a00675e710bb24cf7e518282f9bd1b27834
Juansa77 <dedalus1977@gmail.com>
2023-05-25 09:44:31 +0200

    Added routes

--- Node/SCAFFOLDING/src/api/routes/user.routes.js
+++ Node/SCAFFOLDING/src/api/routes/user.routes.js
@@ -1,2 +1,33 @@
-const express = require("express")
-upload
+const express = require("express");
+const { upload } = require("../../middlewares/files.middleware");
+const {
+  register,
+  checkNewUser,
+  updateUser,
+  deleteUser,
+  resendCode,
+  login,
+  forgotPassword,
+  sendPassword,
+  modifyPassword,
+} = require("../controllers/users.controller");
+const { isAuth } = require("../../middlewares/auth.midddleware")
+
+const UserRoutes = express.Router()
+
+UserRoutes.post("/register", upload.single("image"), register);
+UserRoutes.post("/check", checkNewUser);
+UserRoutes.post("/resend", resendCode);
+UserRoutes.post("/login", login);
+UserRoutes.get("/forgotpassword", forgotPassword);
+UserRoutes.patch("/changepassword", [isAuth], modifyPassword);
+UserRoutes.patch("/update/update", [isAuth], upload.single("image"), updateUser );
+UserRoutes.delete("/", [isAuth] , deleteUser);
+
+//?--------------------
+//*---REDIRECT ROUTE 
+//?--------------------
+
+UserRoutes.get("/forgotpassword/sendpassword/:id", sendPassword)
+
+module.exports = UserRoutes
