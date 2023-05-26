59ba751a2983ffc3c5dff9581312daf68e5fbb26
Juansa77 <dedalus1977@gmail.com>
2023-05-26 11:38:09 +0200

    Added Addgametouser controller and routes

--- Node/SCAFFOLDING/src/api/controllers/games.controller.js
+++ Node/SCAFFOLDING/src/api/controllers/games.controller.js
@@ -6,6 +6,7 @@ const setError = require("../../helpers/handleError");
 const { deleteImgCloudinary } = require("../../middlewares/files.middleware");
 const { generateToken } = require("../../utils/token");
 const randomPassword = require("../../utils/randomPassword");
+const User = require("../models/user.model");
 dotenv.config();
 
 
@@ -51,5 +52,36 @@ const gameByID = async(req, res, next)=>{
 }
 
 
+//!-------------------------------------------
+//?-----------ADD GAME  TO USER------------
+//!-------------------------------------------
 
-module.exports ={title, gameByID}
+const addGameToUser = async(req, res, next) =>{
+
+    const {userId, gameId} = req.params 
+
+    try {
+
+        const user = await User.findById(userId)
+        if(!user){
+            return res.status(404).json({message:"user not found"})
+        }
+        //Hacemos un includes para no meter juegos repetidos
+        if (user.games.includes(gameId)) {
+            return res.status(400).json({ message: "Game already added to user" });
+          }
+
+        else{
+        
+            await user.save()
+            return res.status(200).json({message: "Game added to user"})
+        }
+    } catch (error) {
+        console.log("Error adding game to user", error);
+        return res.status(500).json({ message: "Internal server error" });
+        
+    }
+
+}
+
+module.exports ={title, gameByID, addGameToUser}
--- Node/SCAFFOLDING/src/api/routes/game.routes.js
+++ Node/SCAFFOLDING/src/api/routes/game.routes.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const { upload } = require("../../middlewares/files.middleware");
-const { title, gameByID } = require("../controllers/games.controller");
+const { title, gameByID, addGameToUser } = require("../controllers/games.controller");
 const { isAuth } = require("../../middlewares/auth.midddleware");
 
 const GamesRoutes = express.Router();
 
-
+//Ruta by title--------------------
 GamesRoutes.get("/title/:title", title)
+//Ruta by ID
 GamesRoutes.get("/id/:id", gameByID)
+//Ruta ADD GAME TO USER
+GamesRoutes.post("/:userId/add-game/:gameId",[isAuth], addGameToUser);
+
 
 
 module.exports =GamesRoutes
